8d251cac351689878c5a67720a2795fcc2fb5bd8
aniruddhavpatil <aniruddhavivekpatil@gmail.com>
2020-11-18 23:26:56 -0500

    Changed formatting of console log

--- instacart.js
+++ instacart.js
@@ -1,7 +1,10 @@
 var array = $("div.item-delivered");
-// var array = $("div.item-actually-delivered")
+
+var items = '\n'
 $.each(array, (i, v) => {
-var curr_name = v.children[1].innerHTML.split('\n')[1].replace(/ /g, '_');
-var curr_price = v.children[2].children[0].children[0].innerHTML.replace('$', '');
-console.log(curr_name, curr_price);
+    var curr_name = v.children[1].innerHTML.split('\n')[1].replace(/ /g, '_');
+    var curr_price = v.children[2].children[0].children[0].innerHTML.replace('$', '');
+    items += curr_name + '\t' + curr_price + '\n';
 })
+
+console.log(items);
